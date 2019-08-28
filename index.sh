cd4dad78cd4a70fdd6035da496d6477132a71e7d
Abhinav Anand <abhinav_anand@live.com>
2019-08-28 17:15:29 +0530

    Add shell scripts index.sh and search.sh

---
+++ index.sh
@@ -0,0 +1,10 @@
+if [[ $# -ne 2 ]]; then
+        echo "Pass exactly 2 args <path_to_dump> and <path_to_index_folder>";
+        exit 1;
+fi
+path_to_dump=$1;
+path_to_index_folder=$2;
+echo $path_to_dump;
+echo $path_to_index_folder;
+
+python build_index.py $path_to_dump $path_to_index_folder
---
+++ search.sh
@@ -0,0 +1,10 @@
+if [[ $# -ne 3 ]]; then
+	echo "Pass exactly 3 args <path_to_index_folder> <path_to_input_query_file> <path_to_output_file>.";
+	exit 1;
+fi
+
+path_to_index_folder=$1;
+path_to_input_query_file=$2;
+path_to_output_file=$3;
+
+python search.py $path_to_index_folder $path_to_input_query_file $path_to_output_file
